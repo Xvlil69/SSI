ccaabf1b7448325a7f2beeb910a99cd0fabcf75a
Xvlil69 <hibouseck10@gmail.com>
2026-05-03 14:48:03 +0000

    fix: allow supabase image domain

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  images: {
+    domains: ['hsdlxjyeymroeuatsomx.supabase.co'],
+  },
 }
 
 module.exports = nextConfig
